aa67b968ebf51c59fb4ce44b749b442cfad0392a
hareesh <hareesh.puliyedath@gmail.com>
2015-10-12 23:24:41 -0700

    directive putest he note model on the inner scope

--- public/modules/notes/directives/notes-directive.client.directive.js
+++ public/modules/notes/directives/notes-directive.client.directive.js
@@ -1,50 +1,51 @@
 'use strict';
 
 angular.module('notes').directive('notesDirective', [
-	function() {
-		return {
-			scope:{
-				visible: '@visible',
-				onSubmit: '&',
-				note: '=?note' //this makes the attribute optional
-			},
-			templateUrl: '/modules/notes/views/create-note.client.view.html',
-			restrict: 'E',
-			link: function($scope, element, attrs){
-				$scope.note = $scope.note || {};
-				$scope.show = attrs.show;
-				$scope.disabled = true;
-				element.onclick = function(){
-					$scope.disabled = true;
-				};
-			
-			},
-			controller: function($scope){
-				$scope.submit = function(){
-					//pass name and descriptoin to the parent scop
-					this.onSubmit({note: $scope.note});
-				};
-
-				$scope.update  = function(){
-					$scope.note.update = true;
-					$scope.disabled = true;
-					this.submit();
-				};
-
-				$scope.delete  = function(){
-					$scope.note.delete = true;
-					$scope.disabled = true;
-					this.submit();
-				};
-
-				$scope.toggle = function(){
-					if($scope.disabled){
-						$scope.disabled = false;
-					}
-
-				};
-			},
-			replace: true
+    function() {
+	return {
+	    scope:{
+		visible: '@visible',
+		onSubmit: '&',
+		note: '=?note' //this makes the attribute optional
+	    },
+	    templateUrl: '/modules/notes/views/create-note.client.view.html',
+	    restrict: 'E',
+	    link: function($scope, element, attrs){
+		$scope.note = $scope.note || {};
+		$scope.show = attrs.show;
+		$scope.disabled = true;
+		element.onclick = function(){
+		    $scope.disabled = true;
 		};
-	}
+		
+	    },
+	    controller: function($scope){
+		$scope.toggle = function(){
+		    //pass name and descriptoin to the parent scop
+		    this.onSubmit({note: $scope.note});
+		};
+
+		$scope.update  = function(){
+		    $scope.note.update = true;
+		    $scope.disabled = true;
+		    this.submit();
+		};
+
+		$scope.delete  = function(){
+		    $scope.note.delete = true;
+		    $scope.disabled = true;
+		    this.submit();
+		};
+
+		$scope.toggle = function(){
+		    if($scope.disabled){
+			$scope.disabled = false;
+		    }
+
+		};
+
+	    },
+	    replace: true
+	};
+    }
 ]);
