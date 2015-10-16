ee5a5cc3a33aba80061656b058cfa9604bed81c8
hareesh <hareesh.puliyedath@gmail.com>
2015-10-15 20:15:07 -0700

    saves the oauth profile and enables the authentication

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -1,36 +1,48 @@
 'use strict';
 
-angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Authentication',
-    function($scope, $http, $location, Authentication) {
-        $scope.authentication = Authentication;
+angular.module('users')
+    .controller('AuthenticationController',
+		['$scope', '$http', '$location', 'Authentication','toaster',
+		 function($scope, $http, $location, Authentication, toaster) {
+		     $scope.authentication = Authentication;
 
-        //If user is signed in then redirect back home
-        if ($scope.authentication.user) $location.path('/hareesh');
+		     //If user is signed in then redirect back home
+		     if ($scope.authentication.user) $location.path('/hareesh');
 
-            $scope.signup = function() {
-		$http.post('/auth/signup', $scope.credentials).success(function(response) {
-                //If successful we assign the response to the global user model
-		$scope.authentication.user = response;
+		     $scope.signup = function() {
+			 $http.post('/auth/signup', $scope.credentials).success(function(response) {
+			     //If successful we assign the response to the global user model
+			     $scope.authentication.user = response;
+			     success(true);
+			     //And redirect to the index page
+			     $location.path('/hareesh');
+			 }).error(function(response) {
+			     $scope.error = response.message;
+			     success(false);
+			 });
+		     };
 
-                //And redirect to the index page
-                $location.path('/');
-		}).error(function(response) {
-			$scope.error = response.message;
-		});
-        };
+		     function success(flag){
+			 if(flag){
+			     console.log($scope.authentication.user);
+			     toaster.pop('success', '','Logging in as ' +  $scope.authentication.user.username);
+			 }else{
+			     toaster.pop('error', '',$scope.error);
+			 }
+		     }
 
-        $scope.signin = function() {
-		$http.post('/auth/signin', $scope.credentials).success(function(response) {
+		     $scope.signin = function() {
+			 $http.post('/auth/signin', $scope.credentials).success(function(response) {
 
-                //If successful we assign the response to the global user model
-                $scope.authentication.user = response;
-
-                //And redirect to the index page
-                $location.path('/hareesh');
-		}).error(function(response) {
-			console.log(response);
-                $scope.error = response.message;
-            });
-        };
-    }
-]);
+			     //If successful we assign the response to the global user model
+			     $scope.authentication.user = response;
+			     success(true);
+			     //And redirect to the index page
+			     $location.path('/hareesh');
+			 }).error(function(response) {
+			     $scope.error = response.message;
+			     success(false);
+			 });
+		     };
+		 }
+		]);
