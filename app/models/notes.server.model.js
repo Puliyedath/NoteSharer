300c6aa25aed07d0a20398fd372ca2b09c6e755e
hareesh <hareesh.puliyedath@gmail.com>
2015-10-24 12:38:07 -0700

    adds a field for note sharing

--- app/models/notes.server.model.js
+++ app/models/notes.server.model.js
@@ -4,32 +4,36 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-	Schema = mongoose.Schema;
+    Schema = mongoose.Schema;
 
 /**
  * Note Schema
  */
 var NoteSchema = new Schema({
-	name: {
-		type: String,
-		default: '',
-		required: 'Please fill Note name',
-		trim: true
-	},
-	description: {
-		type: String,
-		default: '',
-		required: 'Please fill Note description',
-		trim: true
-	},
-	created: {
-		type: Date,
-		default: Date.now
-	},
-	user: {
-		type: Schema.ObjectId,
-		ref: 'User'
-	}
+    name: {
+	type: String,
+	default: '',
+	required: 'Please fill Note name',
+	trim: true
+    },
+    description: {
+	type: String,
+	default: '',
+	required: 'Please fill Note description',
+	trim: true
+    },
+    created: {
+	type: Date,
+	default: Date.now
+    },
+    public: {
+	type:Boolean,
+	defautl: false
+    },
+    user: {
+	type: Schema.ObjectId,
+	ref: 'User'
+    }
 });
 
 mongoose.model('Note', NoteSchema);
